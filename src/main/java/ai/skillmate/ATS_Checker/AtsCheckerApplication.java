e7600315211ced43735057b99fb06ea0890e7e29
aashi-Skillmate <aashish@skillmate.ai>
2024-07-30 13:07:06 +0530

    feat(src,pom):configuration

---
+++ src/main/java/ai/skillmate/ATS_Checker/AtsCheckerApplication.java
@@ -0,0 +1,28 @@
+// package ai.skillmate.ATS_Checker;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class AtsCheckerApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(AtsCheckerApplication.class, args);
+		
+// 		System.out.println("hello world");
+// 	}
+
+// }
+
+
+package ai.skillmate.ATS_Checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsCheckerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AtsCheckerApplication.class, args);
+    }
+}
---
+++ src/test/java/ai/skillmate/ATS_Checker/AtsCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.skillmate.ATS_Checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
